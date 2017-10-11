0025694f16142b4c13cf3b670b2f90bd11bbbac8
Syrahl696 <Syrahl696@gmail.com>
2017-10-11 16:16:57 +1100

    Automated test for Bug 1
    Automated test created to reliably reproduce Bug 1 and test whether it has been introduced.

---
+++ test/unit_test/CrownAndAnchorTestSuite.java
@@ -0,0 +1,53 @@
+package unit_test;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runner.notification.Failure;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Ryan Smith
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+GameTest.class})
+public class CrownAndAnchorTestSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+    public static void main(String[] args) {
+      Result result = JUnitCore.runClasses(GameTest.class);
+      
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
+		
+      System.out.println(result.wasSuccessful());
+    }
+}
